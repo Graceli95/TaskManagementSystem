2f86e8c3efb376fed360dd9def781439a82b02b4
Graceli95 <graceli9095@gmail.com>
2025-03-04 07:36:21 -0700

    created model and repo

---
+++ src/main/java/com/grace/taskmanagementsystem/repository/TaskRepository.java
@@ -0,0 +1,7 @@
+package com.grace.taskmanagementsystem.repository;
+
+import com.grace.taskmanagementsystem.model.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TaskRepository extends JpaRepository<Task, Long> {
+}
---
+++ src/main/java/com/grace/taskmanagementsystem/service/TaskService.java
@@ -0,0 +1,4 @@
+package com.grace.taskmanagementsystem.service;
+
+public class TaskService {
+}
