a554ee094e0351f84469597cbec01eddb63459f3
Graceli95 <graceli9095@gmail.com>
2025-03-04 07:48:46 -0700

    set up TaskService

--- src/main/java/com/grace/taskmanagementsystem/repository/TaskRepository.java
+++ src/main/java/com/grace/taskmanagementsystem/repository/TaskRepository.java
@@ -2,6 +2,8 @@ package com.grace.taskmanagementsystem.repository;
 
 import com.grace.taskmanagementsystem.model.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
 }
--- src/main/java/com/grace/taskmanagementsystem/service/TaskService.java
+++ src/main/java/com/grace/taskmanagementsystem/service/TaskService.java
@@ -1,4 +1,47 @@
 package com.grace.taskmanagementsystem.service;
 
+import com.grace.taskmanagementsystem.model.Task;
+import com.grace.taskmanagementsystem.repository.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class TaskService {
+
+    private TaskRepository taskRepository;
+
+    @Autowired
+    public TaskService(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+    public List<Task> getAllTasks(){
+        return taskRepository.findAll();
+    }
+
+    public Optional getTaskById(Long id){
+        return taskRepository.findById(id);
+    }
+
+    public Task createTask(Task task){
+        return taskRepository.save(task);
+    }
+
+    public Task updateTask(Long id, Task updatedTask){
+        return taskRepository.findById(id)
+                .map(task -> {
+                    task.setTitle(updatedTask.getTitle());
+                    task.setDescription(updatedTask.getDescription());
+                    return taskRepository.save(task);
+                })
+                .orElseThrow(() -> new RuntimeException("Task not found"));
+    }
+
+    public void deleteTask(Long id){
+        taskRepository.deleteById(id);
+    }
+
 }
